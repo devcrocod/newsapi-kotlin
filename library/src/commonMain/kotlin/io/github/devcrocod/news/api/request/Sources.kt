a9fb14125e1494689bfc33f8d0188e48e8ae08eb
devcrocod <devcrocod@gmail.com>
2024-02-19 13:34:05 +0100

    Set default values for NewsApiClient source parameters

--- library/src/commonMain/kotlin/io/github/devcrocod/news/api/request/Sources.kt
+++ library/src/commonMain/kotlin/io/github/devcrocod/news/api/request/Sources.kt
@@ -7,7 +7,7 @@ import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 
-public suspend fun NewsApiClient.sources(body: SourcesRequest.() -> Unit): List<Source> {
+public suspend fun NewsApiClient.sources(body: SourcesRequest.() -> Unit = {}): List<Source> {
     val res: Response<Source> = this.client.get("top-headlines/sources") {
         url { parameters.appendAll(SourcesRequest().apply(body).build().getParameters()) }
     }.body()
