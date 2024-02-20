a812e5b292d9017f83d05ab3e51683aaddd2318c
devcrocod <devcrocod@gmail.com>
2024-02-21 00:05:17 +0100

    Add authentication for Sonatype repository

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -11,6 +11,8 @@ nexusPublishing {
     // Configure maven central repository
     repositories {
         sonatype {
+            username = project.findProperty("OSSRH_USERNAME") as String?
+            password = project.findProperty("OSSRH_PASSWORD") as String?
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
         }
